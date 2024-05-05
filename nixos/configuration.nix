daedbded8f82bc453101a10974141f990d37592c
vpletea <84437690+vpletea@users.noreply.github.com>
2024-05-05 17:46:25 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -294,30 +294,6 @@
      };
   };
 
-
-
-#  services.power-profiles-daemon.enable = false;
-
-#  services.tlp = {
-#        enable = true;
-#        settings = {
-#          CPU_SCALING_GOVERNOR_ON_AC = "performance";
-#          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-#
-#          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-#          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-#          CPU_MIN_PERF_ON_AC = 0;
-#          CPU_MAX_PERF_ON_AC = 100;
-
-#          CPU_MIN_PERF_ON_BAT = 0;
-#          CPU_MAX_PERF_ON_BAT = 25;          
-
-#          CPU_BOOST_ON_AC = 1;
-#          CPU_BOOST_ON_BAT = 0;
-
-#        };
-#  };  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
